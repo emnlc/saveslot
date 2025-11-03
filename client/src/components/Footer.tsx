13ff4e7403ae1015e379b71f75b580b1b46018ec
emmanuel cortes <emnlc.dev@gmail.com>
2025-11-03 12:11:09 -0700

    logo

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -6,13 +6,20 @@ const Footer = () => {
     <>
       <footer className="w-full bg-base-200/80 py-6 self-end">
         <div className="container mx-auto flex flex-row items-center justify-between gap-4 px-4 md:px-0">
-          <div>
-            <div className="flex items-start md:items-center gap-2">
-              <Link to="/" className="hover:brightness-[90%] transition-all">
-                <span className="font-semibold">
-                  <span className="text-primary">Save</span>Slot
-                </span>
-              </Link>
+          <div className="flex flex-col gap-1">
+            <div className="flex flex-row gap-1 md:gap-2">
+              <img
+                src="/saveslot.svg"
+                alt="SaveSlot logo"
+                className="w-4 md:w-6"
+              />
+              <div className="flex items-start md:items-center gap-2">
+                <Link to="/" className="hover:brightness-[90%] transition-all">
+                  <span className="font-semibold">
+                    <span className="text-primary">Save</span>Slot
+                  </span>
+                </Link>
+              </div>
             </div>
             <p className="text-left text-xs md:text-sm text-muted-foreground ">
               &copy; {new Date().getFullYear()} SaveSlot. All rights reserved.
