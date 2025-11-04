768c91ed3fb2c3be2be4efc801f5542209794d4f
emmanuel cortes <emnlc.dev@gmail.com>
2025-11-04 15:57:42 -0700

    footer update

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,11 +1,10 @@
 import { Link } from "@tanstack/react-router";
-import { Github } from "lucide-react";
 
 const Footer = () => {
   return (
     <>
-      <footer className="w-full bg-base-200/80 py-6 self-end">
-        <div className="container mx-auto flex flex-row items-center justify-between gap-4 px-4 md:px-0">
+      <footer className="w-full bg-base-200/80 py-12 self-end">
+        <div className="container mx-auto flex flex-row items-end justify-between gap-4 px-4 md:px-0">
           <div className="flex flex-col gap-1">
             <div className="flex flex-row gap-1 md:gap-2">
               <img
@@ -22,19 +21,11 @@ const Footer = () => {
               </div>
             </div>
             <p className="text-left text-xs md:text-sm text-muted-foreground ">
-              &copy; {new Date().getFullYear()} SaveSlot. All rights reserved.
+              &copy; {new Date().getFullYear()} SaveSlot
             </p>
           </div>
+
           <div className="flex flex-col items-end gap-2">
-            <div>
-              <a
-                className="hover:text-primary transition-colors"
-                target="_blank"
-                href="https://github.com/emnlc/saveslot"
-              >
-                <Github className="h-4 w-4 md:h-5 md:w-5" />
-              </a>
-            </div>
             <span className="text-xs md:text-sm">
               Powered by{" "}
               <a
